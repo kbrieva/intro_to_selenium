d2d341e725fc7fed5fa888688a8a64db93c7ea01
Kennedy <121739110+kbrieva@users.noreply.github.com>
2024-03-04 13:17:39 +0800

    Adding modified code

--- pages/search.py
+++ pages/search.py
@@ -2,17 +2,18 @@
 This module contains DuckDuckGoSearchPage,
 the page object for the DuckDuckGo search page.
 """
+from selenium.webdriver.common.by import By
 
 
 class DuckDuckGoSearchPage:
+    SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
+    def __init__(self, browser):
+        self.browser = browser
 
-  def __init__(self, browser):
-    self.browser = browser
+    def load(self):
+        # TODO
+        pass
 
-  def load(self):
-    # TODO
-    pass
-
-  def search(self, phrase):
-    # TODO
-    pass
+    def search(self, phrase):
+        # TODO
+        pass
